14f5fff2c901f4b7e010a78daec60dc8613bcf3b
Markus Hoesel <markus.hoesel@mobilmail.at>
2015-08-16 17:05:14 +0200

    Editing Exercise File 6.3

--- src/main/scala-2.11/at/hoeselm/exercises6/Exercise3.scala
+++ src/main/scala-2.11/at/hoeselm/exercises6/Exercise3.scala
@@ -1,16 +1,40 @@
 package at.hoeselm.exercises6
 
-// TODO: Add FoldLeft and ForLoop
+// TODO: Needs revise
 
 object Exercise3 {
 
   def main(args: List[String]): Unit = {
-    println(first(List('a', 't', 'o', 's'), 2))
+
+    println(first_take(List('a', 't', 'o', 's'), 2))
+    println(first_loop(List('a', 't', 'o', 's'), 2))
+    println(first_recursive(List('a', 't', 'o', 's'), 2))
+
+  }
+  
+  def first_take[A](items: List[A], count: Int): List[A] = {
+    
+    items.take(3)
+    
+  }
+  
+  def first_fold_left[A](items: List[A], count: Int): List[A] = {
+    
+    items.foldLeft[List[A]](Nil)({
+      (a: List[A], i: A) => if (a.size >= count) a else i :: a})
+    
   }
 
-  def first[A](items: List[A], count: Int): List[A] = {
+  def first_loop[A](items: List[A], count: Int): List[A] = {
 
-    if (count > 0) items.head :: first(items.tail, count - 1)
+    val result = for (i <- 0 to count) yield items(i)
+    result.toList
+
+  }
+
+  def first_recursive[A](items: List[A], count: Int): List[A] = {
+
+    if (count > 0) items.head :: first_recursive(items.tail, count - 1)
     else Nil
 
   }
