aee313b1c385369695ffd9e3dff7d297ebc8c95f
Markus Hoesel <markus.hoesel@mobilmail.at>
2015-08-20 18:19:45 +0200

    Editing Exercise 7.6

--- src/main/scala-2.11/at/hoeselm/exercises7/Exercise6.scala
+++ src/main/scala-2.11/at/hoeselm/exercises7/Exercise6.scala
@@ -4,15 +4,17 @@ object Exercise6 {
 
   def main(args: List[String]): Unit = {
 
-    getGithubRssEntries("scala", "scala", "2.11.x").foreach { x => println(findProperties(x).getOrElse("not available")) }
-
+    //getGithubRssEntries("scala", "scala", "2.11.x").foreach { x => println(findProperties(x).getOrElse("not available")) }
+    //getGithubRssEntriesTuple(("scala", "scala", "2.11.x")).foreach { x => println(findProperties(x).getOrElse("not available")) }
+    getGithubRssEntriesTupleList(List(("scala", "scala", "2.11.x"),("hoeselm", "kicklog", "master"),("scala", "scala", "2.11.x"))).foreach { x => println(findProperties(x).getOrElse("not available")) }
   }
 
+  // main
   def getGithubRssEntries(username: String, reponame: String, branchname: String): List[String] = {
-    //val u = s"https://github.com/$username/$reponame/commits/$branchname.atom"
-    //val s = io.Source.fromURL(u)
-    //val text = s.getLines.map(_.trim).mkString("")
-    val text = scala.io.Source.fromFile("/home/ikthsm/githubrss.xml").getLines.mkString
+    val u = s"https://github.com/$username/$reponame/commits/$branchname.atom"
+    val s = io.Source.fromURL(u)
+    val text = s.getLines.map(_.trim).mkString("")
+    //val text = scala.io.Source.fromFile("/home/ikthsm/githubrss.xml").getLines.mkString
     val entries = text.split("</?entry>").toList.tail
     val filtered_entries = entries.filter { !_.isEmpty() }
     filtered_entries
@@ -27,6 +29,20 @@ object Exercise6 {
       case None => None
     }
 
+  }
+  
+  // a
+  def getGithubRssEntriesTuple(identifier: ( String, String, String)): List[String] = {
+    
+    getGithubRssEntries(identifier._1,identifier._2,identifier._3)
+    
+  } 
+  
+  // b
+  def getGithubRssEntriesTupleList(identifiers: List[(String,String,String)]) :List[String] = {
+    
+    identifiers.flatMap(getGithubRssEntriesTuple)
+    
   }
 
 }
