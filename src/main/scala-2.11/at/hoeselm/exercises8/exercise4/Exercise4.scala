89b3feb3e324679f1fad25ff27e01e6c3b867b54
Markus Hoesel <markus.hoesel@mobilmail.at>
2015-08-23 13:59:46 +0200

    Updating Exercise 8.4

--- src/main/scala-2.11/at/hoeselm/exercises8/exercise4/Exercise4.scala
+++ src/main/scala-2.11/at/hoeselm/exercises8/exercise4/Exercise4.scala
@@ -5,6 +5,7 @@ object Exercise4 {
   def main(args: List[String]): Unit = {
 
     new Mozart(100).play(Seq(60, 62, 64, 65, 67, 69, 71, 72))
+    new Mozart(100).play((30 to 70 by 5) ++ (70 to 30 by -5))
 
   }
 
--- src/main/scala-2.11/at/hoeselm/exercises8/exercise4/Mozart.scala
+++ src/main/scala-2.11/at/hoeselm/exercises8/exercise4/Mozart.scala
@@ -2,20 +2,20 @@ package at.hoeselm.exercises8.exercise4
 
 class Mozart(val volume: Int) {
 
-  val duration = 250
+  private val duration = 250
 
   def play(notes: Seq[Int]) = {
 
-    val synth = javax.sound.midi.MidiSystem.getSynthesizer
+    lazy val synth = javax.sound.midi.MidiSystem.getSynthesizer
     synth.open()
-    val channel = synth.getChannels.head
+    lazy val channel = synth.getChannels.head
 
     for (note <- notes) {
       channel.noteOn(note, volume)
       Thread.sleep(duration)
       channel.noteOn(note, 0)
     }
-    
+
     synth.close()
 
   }
