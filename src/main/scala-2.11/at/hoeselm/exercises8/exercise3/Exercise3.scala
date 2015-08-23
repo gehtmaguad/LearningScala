d04de8b3806f823b1cee29818690d5b70be80cf1
Markus Hoesel <markus.hoesel@mobilmail.at>
2015-08-23 10:56:20 +0200

    Updating Exercise File 8.3

--- src/main/scala-2.11/at/hoeselm/exercises8/exercise3/Exercise3.scala
+++ src/main/scala-2.11/at/hoeselm/exercises8/exercise3/Exercise3.scala
@@ -1,5 +1,15 @@
 package at.hoeselm.exercises8.exercise3
 
 object Exercise3 {
-  
+
+  def main(args: List[String]): Unit = {
+
+    def filterTextfiles(name: String): Boolean = {
+      !name.endsWith(".txt")
+    }
+
+    println(new DirectoryListing(".", filterTextfiles).list)
+
+  }
+
 }
